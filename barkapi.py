d26fffce1bee2550715028074ef49d8ee9ed1b5f
Meatfucker <tramshed@gmail.com>
2023-08-30 03:44:41 -0400

    Updated to no longer use a temporary file

--- barkapi.py
+++ barkapi.py
@@ -1,20 +1,20 @@
 from bark import SAMPLE_RATE, generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
 import random
+import io
 
 preload_models()
-
 barkapi = FastAPI()
 
 @barkapi.get("/txt2wav")
 def txt2wav(inputstring: str):
-    text_prompt = inputstring
-    audio_array = generate_audio(text_prompt, history_prompt="voices/matlighty.npz")
-    filename = f"{inputstring}-{random.randint(1000, 9999)}.wav"  
-    write_wav(filename, SAMPLE_RATE, audio_array)
-    return FileResponse(filename, media_type="audio/wav")
+    audio_array = generate_audio(inputstring, history_prompt="voices/matlighty.npz")
+    wav_io = io.BytesIO()
+    write_wav(wav_io, SAMPLE_RATE, audio_array)
+    wav_io.seek(0)
+    return StreamingResponse(wav_io, media_type="audio/wav")
 
 if __name__ == "__main__":
     import uvicorn
